fec56846535366a8fc4b85ecde13f91d2b5e2ff5
leor0104 <luisortiz141999@gmail.com>
2022-03-14 10:00:32 -0500

    Add Sidenav on Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Navbar from '@components/Navbar/Navbar'
+import Sidenav from '@components/Sidenav/Sidenav'
 
 type LayoutProps = {
   children?: React.ReactNode
 }
 
-const Layout = ({ children }: LayoutProps) => (
-  <React.Fragment>
-    <Navbar />
+function Layout({ children }: LayoutProps) {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Sidenav/>
       {children}
-  </React.Fragment>
-)
+    </React.Fragment>
+  )
+}
 
 export default Layout
