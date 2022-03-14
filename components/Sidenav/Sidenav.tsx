6e9b7302afcb42046418d765f62154d90d3b3867
leor0104 <luisortiz141999@gmail.com>
2022-03-14 09:58:22 -0500

    Add Sidenav

---
+++ components/Sidenav/Sidenav.tsx
@@ -0,0 +1,15 @@
+import styles from '@components/Sidenav/Sidenav.module.css'
+import { useRouter } from 'next/router';
+
+function Sidenav() {
+    const { pathname } = useRouter();
+    return (
+        <div className={styles.sidenav}>
+            <a href="/new">Nuevos</a>
+            <a href="/new">Presentaciones</a>
+            <a href="/new">Tips</a>
+            <a href="/new">Plantillas</a>
+        </div>
+    );
+}
+export default Sidenav;
