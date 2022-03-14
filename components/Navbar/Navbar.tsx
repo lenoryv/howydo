edfa929afeab694f1d9cd179f2caf6d043190cc0
leor0104 <luisortiz141999@gmail.com>
2022-03-13 21:09:34 -0500

    Add styles Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,29 @@
 import styles from '@components/Navbar/Navbar.module.css'
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 function Navbar() {
     const { pathname } = useRouter()
     return (
-        <div className={styles.navbar} id="myNavbar">
-            <a className={pathname === "/" ? styles.active : ""} href="/">
-                Home
-            </a>
-            <a className={pathname === "/about" ? styles.active : ""} href="/about">
-                About
-            </a>
-            <a href="/contact">Contact</a>
-
-            <a href="/howydo" className="icon"></a>
-        </div>
+        <ul className={styles.navbar} id="myNavbar">
+            <div>
+                Howydo
+            </div>
+            <li>
+                <a className={pathname === "/" ? styles.active : ""} href="/">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a className={pathname === "/about" ? styles.active : ""} href="/about">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="/contact">
+                    Contact
+                </a>
+            </li>
+        </ul>
     )
 }
 export default Navbar;
