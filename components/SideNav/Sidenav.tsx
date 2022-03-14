73771a6e3e545ee003f7bc8af8cad83366c5836f
leor0104 <luisortiz141999@gmail.com>
2022-03-14 09:36:03 -0500

    Add option sidenav

--- components/SideNav/Sidenav.tsx
+++ components/SideNav/Sidenav.tsx
@@ -5,9 +5,10 @@ function Sidenav() {
     const { pathname } = useRouter()
     return (
         <div className={styles.sidenav}>
-            <a href="/new">New</a>
+            <a href="/new">Nuevos</a>
             <a href="/new">Presentaciones</a>
             <a href="/new">Tips</a>
+            <a href="/new">Plantillas</a>
         </div>
     )
 }
