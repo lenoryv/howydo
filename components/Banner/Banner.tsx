f9d88a4cfb7d55fc36fa3e7de0923703d7233832
leor0104 <luisortiz141999@gmail.com>
2022-03-14 20:22:43 -0500

    Create Banner

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,53 @@
+import styles from '@components/Banner/Banner.module.css'
+import CardHype from '@components/CardHype/CardHype';
+import Divider from '@components/Divider/Divider';
+
+const hype = '/images/CamisetaIND.jpg'
+const img = '/images/Dog.jpg';
+const nets = [
+  {
+    name: 'linkedin',
+    icon: '/icons/linkedin48.png',
+  },
+  {
+    name: 'instagram',
+    icon: '/icons/instagram48.png',
+  },
+  {
+    name: 'github',
+    icon: '/icons/github64.png',
+  }
+]
+
+function Banner() {
+  return (
+    <div className={styles.banner}>
+            <div className={styles.sponsors}>
+        Add Colaborator or Auspiciants
+      </div>
+      <div className={styles.hype}>
+        <CardHype img={hype} />
+      </div>
+      <Divider />
+      <div className={styles.profile}>
+        <div className={styles.dot}>
+          <img src={img} width={72} height={72} />
+        </div>
+        <div className={styles.boxText}>
+          <p>Luis Enrique Ortiz Rivera</p>
+          <p>Developer and Creator</p>
+        </div>
+        <div className={styles.buttonGroup}>
+          {nets.map((net) => {
+            return (
+              <div className={styles.iconButton} key={net.name}>
+                <img src={net.icon} width={24} height={24} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Banner;
