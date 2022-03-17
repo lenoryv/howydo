97b4ec2e18cd4e784561593525785c8d114fb814
leor0104 <luisortiz141999@gmail.com>
2022-03-16 19:31:57 -0500

    Create query a database

---
+++ pages/api/howydos.tsx
@@ -0,0 +1,30 @@
+const { Client } = require('@notionhq/client');
+
+// Initializing a client
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+export default async function getHyds() {
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+                property: 'Category',
+                multi_select: {
+                    contains: 'tip'
+                    
+                }
+            },
+        });
+        console.log('Res: ', response.results);
+        return {
+            props: {
+              howydos: response.results,
+            },
+            revalidate: 1,
+          };
+    } catch (error) {
+        console.log('Error')
+        
+    }
+}
