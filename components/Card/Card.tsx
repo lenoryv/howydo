2e912885ce988695bd47c31987e797dafa331988
leor0104 <luisortiz141999@gmail.com>
2022-03-23 17:10:53 -0500

    Dinamic Pages

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '@components/Card/Card.module.css';
+import Link from "next/link";
 
 const arrowIcon = '/icons/arrow-24.png';
 
@@ -13,12 +14,13 @@ function Card({howydos}) {
                         <h2>{howydo.properties.Name.title[0].plain_text}</h2>
                         <h3>{howydo.properties.Date.date.start}</h3>
                         <p>{howydo.properties.Description.rich_text[0].plain_text}</p>
-                        <div className={styles.button}>
+                        <Link href={'/howydo/'+ howydo.properties.Page.relation[0].id}><div className={styles.button}>
+                            
                             <span>Continuar leyendo</span>
                             <div>
                                 <img src={arrowIcon} width={16} height={16}/>
                             </div>
-                        </div>
+                        </div></Link>
                         <hr></hr>
                     </div>
                 )
