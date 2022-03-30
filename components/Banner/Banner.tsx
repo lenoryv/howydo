64a6ee478b7780cae50859fc3bb44bfb4364816b
leor0104 <luisortiz141999@gmail.com>
2022-03-29 21:55:58 -0500

    Banner responsive

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,5 +1,4 @@
-import styles from '@components/Banner/Banner.module.css'
-import CardHype from '@components/CardHype/CardHype';
+import Carousel from '@components/Carousel/Carousel';
 import Divider from '@components/Divider/Divider';
 import Image from 'next/image'
 
@@ -22,31 +21,25 @@ const nets = [
 
 function Banner() {
   return (
-    <div className="h-auto md:w-64 mt-0 md:ml-0 md:m-6 md:col-start-3 md:col-end-4 md:row-start-2 md:row-end-3 m-6 bg-sky-50 rounded-md p-4 flex flex-col justify-between">
-      <div className={styles.hype}>
-        <CardHype img={hype} />
-      </div>
+    <div className="h-fit p-6 md:p-6 md:sticky md:top-20 md:h-fit md:w-80 mt-0 md:ml-0 md:m-6 md:col-start-3 md:col-end-4 md:row-start-2 md:row-end-3 m-6 bg-gray-50 rounded-md span-4 flex flex-col justify-between">
+      <Carousel />
       <Divider />
-      <div className={styles.profile}>
-        <div className={styles.dot}>
-          <Image src={img} width={72} height={72} alt={'hype'} />
-        </div>
-        <div className={styles.boxText}>
-          <p>Luis Enrique Ortiz Rivera</p>
-          <p>Developer and Creator</p>
-        </div>
-        <div className={styles.buttonGroup}>
+      <div className="flex flex-col items-center justify-center">
+        <Image className="rounded-full" src={img} width={72} height={72} alt={'hype'} />
+        <span className="mt-4 text-md">Luis Enrique Ortiz Rivera</span>
+        <span className="font-semibold text-sm">Developer and Creator</span>
+        <div className="flex my-4 items-center align-center">
           {nets.map((net) => {
             return (
-              <div className={styles.iconButton} key={net.name}>
-                <img src={net.icon} width={24} height={24} />
-              </div>
+              <button key={net.name} type="button" className="mr-2 md:mr-0 last:mr-0 hover:bg-sky-100 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+                <img className="w-5 h-5" src={net.icon}/>
+              </button>
             )
           })}
         </div>
       </div>
-      <div className={styles.sponsors}>
-        + Add Colaborator or Auspiciants
+      <div className="w-full md:my-0 flex rounded p-2 font-normal text-gray-400 align-center text-sm h-16 md:w-full items-center bg-white">
+       <span>+ Add Colaborator or Auspiciants</span>
       </div>
     </div>
   )
